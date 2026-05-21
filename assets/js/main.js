a257026489c1f77151d4055c155c9d6f292ce2f9
hsojfp <josh.patinof@gmail.com>
2026-05-22 03:16:44 +0800

    edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,9 +26,65 @@
 		});
 
 	// Scrolly.
-		$('#nav a, .scrolly').scrolly({
+		$('#nav a, .site-header a, .scrolly').scrolly({
 			speed: 1000,
 			offset: function() { return $nav.height(); }
+		});
+
+	// Mobile nav toggle.
+		var $navToggle = $nav.find('.nav-toggle'),
+			$navMenu = $('#nav-menu');
+
+		$navToggle.on('click', function() {
+			var open = $nav.toggleClass('nav-open').hasClass('nav-open');
+			$navToggle.attr('aria-expanded', open);
+			$navToggle.attr('aria-label', open ? 'Close menu' : 'Open menu');
+		});
+
+		$navMenu.find('a').on('click', function() {
+			$nav.removeClass('nav-open');
+			$navToggle.attr('aria-expanded', 'false');
+			$navToggle.attr('aria-label', 'Open menu');
+		});
+
+	// Lightbox for gallery images.
+		var $lightbox = $('#lightbox'),
+			$lightboxImg = $('#lightbox-img'),
+			$lightboxCaption = $('#lightbox-caption');
+
+		function openLightbox(src, caption, alt) {
+			$lightboxImg.attr({ src: src, alt: alt || caption || 'Enlarged visualization' });
+			$lightboxCaption.text(caption || '');
+			$lightbox.removeAttr('hidden');
+			$body.css('overflow', 'hidden');
+		}
+
+		function closeLightbox() {
+			$lightbox.attr('hidden', 'hidden');
+			$lightboxImg.attr({ src: '', alt: '' });
+			$body.css('overflow', '');
+		}
+
+		$('.gallery-trigger').on('click', function() {
+			var $btn = $(this),
+				src = $btn.data('src') || $btn.find('img').attr('src'),
+				caption = $btn.data('caption') || '',
+				alt = $btn.find('img').attr('alt');
+			openLightbox(src, caption, alt);
+		});
+
+		$lightbox.find('.lightbox-close').on('click', closeLightbox);
+
+		$lightbox.on('click', function(e) {
+			if (e.target === $lightbox.get(0)) {
+				closeLightbox();
+			}
+		});
+
+		$(document).on('keydown', function(e) {
+			if (e.key === 'Escape' && !$lightbox.is('[hidden]')) {
+				closeLightbox();
+			}
 		});
 
 })(jQuery);
